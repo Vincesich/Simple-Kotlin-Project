c46aea96a38a18fcb19777bca5ac29387cb0a721
Vince <vince.sich@gmail.com>
2024-02-18 15:17:58 +0300

    Added navigation between my splash and my login and register pages,also text views and buttons in my login and register pages

--- app/src/main/java/com/example/simplekotlinproject/Splash.kt
+++ app/src/main/java/com/example/simplekotlinproject/Splash.kt
@@ -1,11 +1,24 @@
 package com.example.simplekotlinproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Splash: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
+
+        val gotologinpagebutton = findViewById<Button>(R.id.gotologinpagebutton)
+        gotologinpagebutton.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        val gotoregisterpagebutton = findViewById<Button>(R.id.gotoregisterpagebutton)
+        gotoregisterpagebutton.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
