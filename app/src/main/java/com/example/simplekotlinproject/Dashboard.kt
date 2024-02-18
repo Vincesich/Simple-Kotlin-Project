6531092f99130dc10088943e77e2d9b95dce3737
Vince <vince.sich@gmail.com>
2024-02-18 15:29:40 +0300

    Added navigation between dashboard and userlist,help and about screens

--- app/src/main/java/com/example/simplekotlinproject/Dashboard.kt
+++ app/src/main/java/com/example/simplekotlinproject/Dashboard.kt
@@ -1,11 +1,28 @@
 package com.example.simplekotlinproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
+        val gotouserslistbutton = findViewById<Button>(R.id.gotouserslistbutton)
+        gotouserslistbutton.setOnClickListener{
+            val intent = Intent(this, UsersList::class.java)
+            startActivity(intent)
+        }
+        val gotoaboutbutton = findViewById<Button>(R.id.gotoaboutbutton)
+        gotoaboutbutton.setOnClickListener{
+            val intent = Intent(this, About::class.java)
+            startActivity(intent)
+        }
+        val gotohelpbutton = findViewById<Button>(R.id.gotohelpbutton)
+        gotohelpbutton.setOnClickListener{
+            val intent = Intent(this, Help::class.java)
+            startActivity(intent)
+        }
     }
 }
