fc1dadfdbf207f933203a1711c0116b926f56ed2
Vince <vince.sich@gmail.com>
2024-02-18 15:36:14 +0300

    Onclicklisteners on on both my Login and Register to direct the user to the dashboard

--- app/src/main/java/com/example/simplekotlinproject/Login.kt
+++ app/src/main/java/com/example/simplekotlinproject/Login.kt
@@ -1,11 +1,19 @@
 package com.example.simplekotlinproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Login: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val loginbutton = findViewById<Button>(R.id.loginbutton)
+        loginbutton.setOnClickListener{
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/simplekotlinproject/Register.kt
+++ app/src/main/java/com/example/simplekotlinproject/Register.kt
@@ -1,11 +1,19 @@
 package com.example.simplekotlinproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+        val registerbutton = findViewById<Button>(R.id.registerbutton)
+        registerbutton.setOnClickListener{
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+        }
     }
 }
