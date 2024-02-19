6dd0b313db78089a3f43f219cbb689adc22bcfdb
Vince <vince.sich@gmail.com>
2024-02-19 14:31:04 +0300

    Addition of a view model class

--- app/src/main/java/com/example/simplekotlinproject/Register.kt
+++ app/src/main/java/com/example/simplekotlinproject/Register.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
 class Register : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
---
+++ app/src/main/java/com/example/simplekotlinproject/ViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.simplekotlinproject
+
+class ViewModel {
+}
